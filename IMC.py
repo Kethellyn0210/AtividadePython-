3f758c86c8a8f6ee5b4659951c573ac35de576d3
Kethellyn0210 <ketf270@gmail.com>
2025-05-22 15:27:26 -0300

    IMC.py

--- IMC.py
+++ IMC.py
@@ -1,22 +1,22 @@
 peso = float(input("Digite seu peso: "))
 altura = float(input("Digite sua altura: "))
 
-imc = peso / (altura * altura)
-print("Seu Imc é: ", imc)
+imc = peso / (altura ** 2)
+print("Seu Imc é: ", (round(imc,1)))
 
-if imc <16:
+if imc >16:
     print("Magreze grave")
-elif imc <=16.1 and imc >=17:
+elif imc >=16.1 and imc <=17:
     print("Magreza moderada")
-elif imc <=17.1 and imc >=18.5:
+elif imc >=17.1 and imc <=18.5:
     print("Magreza leve")
-elif imc <=18.6 and imc >=25:
+elif imc >=18.6 and imc <=25:
     print("Saudável")
-elif imc <=25.1 and imc >=30:
+elif imc >=25.1 and imc <=30:
     print("Sobre peso")
-elif imc <=30.1 and imc >=35:
+elif imc >=30.1 and imc <=35:
     print("Obesidade grau I")
-elif imc <=35.1 and imc >=40:
+elif imc >=35.1 and imc <=40:
     print("Obesidade grau II")
 else:
     print("Obesidade grau III")
